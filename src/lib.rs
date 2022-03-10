9c4ad3e03e8f6ea1afbe5f36af0b2a5bbeedb3b9
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-03-10 13:09:57 +0100

    add crate description

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! Collection of many various encrypting functions implementations
+//! in pure Rust. Every contained encryption method has its own module
+//! with functions for encrypting and decrypting appropriate type of message.
+
 pub mod ceasar;
 pub mod substitution;
 
