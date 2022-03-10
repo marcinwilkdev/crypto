a6d657324891d22474c0b5b64b2e4baec00e4e7d
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-03-10 13:06:39 +0100

    add modules description

--- src/substitution.rs
+++ src/substitution.rs
@@ -1,3 +1,5 @@
+//! Methods for encrypting and decrypting messages using substitution cipher.
+
 use crate::check_message_from_letters;
 
 /// Encrypts `message` by substitution according to `permutation`.
